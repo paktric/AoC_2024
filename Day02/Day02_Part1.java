cec75a4abfb9a50c1dff3b1f01d58e2fa877ea3d
Patrick Moran <37051272+Paktric@users.noreply.github.com>
2024-12-04 23:56:37 -0500

    Day02 Part1

---
+++ Day02/Day02_Part1.java
@@ -0,0 +1,65 @@
+// Advent of Code 2024, Day 02, Part 1
+// Patrick Moran
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import static java.lang.Math.abs;
+
+public class Day02_Part1 {
+    public static void main(String[] args) {
+        // Instantiate an ArrayList to hold the data reports
+        ArrayList<ArrayList<Integer>> dataReports  = new ArrayList<>();
+
+        // Open "input.txt" and fill the dataReports ArrayLists
+        File file = new File("Day02/input.txt");
+        try {
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()){
+                // Get the current line as a String, split into individual Strings
+                String[] temp = scanner.nextLine().split(" ");
+
+                // Fill dataset ArrayList with the individual Strings convered to Integers
+                ArrayList<Integer> dataset = new ArrayList<>();
+                for (int i = 0; i < temp.length; i++) {
+                    dataset.add(Integer.valueOf(temp[i]));
+                }
+
+                // Add the completed dataset ArrayList to the dataReports ArrayList
+                dataReports.add(dataset);
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        // Review each dataReport dataset and count the 'safe' reports
+        int safeReports = 0;
+        // view dataReports
+        for (int i = 0; i < dataReports.size(); i++) {
+            // view datasets
+            boolean ascending = true;
+            boolean descending = true;
+            boolean difference = true;
+            for (int j = 0; j < dataReports.get(i).size()-1 && (ascending || descending ) && difference; j++) {
+//                System.out.print(dataReports.get(i).get(j) + ", ");
+                int current = dataReports.get(i).get(j);
+                int next = dataReports.get(i).get(j+1);
+
+                System.out.println(current + ", " + next);
+
+                ascending = ascending && (current <= next);
+                descending = descending && (current >= next);
+                difference = difference && (abs(current - next) >= 1 && abs(current - next) <= 3);
+            }
+            System.out.println();
+            if ((ascending || descending) && difference){
+                safeReports++;
+            }
+        }
+
+        // Fin!
+        System.out.println("AoC Day 02, Part 1 answer: " + safeReports);
+    }
+}
